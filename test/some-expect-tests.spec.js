14d52cc926478f72e74a420760b39b5567f5d8b2
Kristofer Selbekk <kristofer.selbekk@sparebank1.no>
2018-02-08 15:12:45 +0100

    Initial commit

---
+++ test/some-expect-tests.spec.js
@@ -0,0 +1,12 @@
+import expect from 'expect.js';
+
+const multiply = (a, b) => a * b;
+
+describe('test suite', () => {
+    it('puts 2 and 2 together', () => {
+        expect(multiply(2, 2)).to.be(4);
+    });
+    it('throws an error if given non-numbers', () => {
+        expect(() => multiply).withArgs({}, 'not a number').to.throwError();
+    });
+});
